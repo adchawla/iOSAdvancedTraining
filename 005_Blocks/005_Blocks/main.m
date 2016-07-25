95c6f7d89cfe79b06c565c68d07e6b420f0fe511
Amandeep Chawla <amandeep@adobe.com>
2016-07-25 15:42:05 +0530

    Using __block attribute to change the variable value in the block

--- 005_Blocks/005_Blocks/main.m
+++ 005_Blocks/005_Blocks/main.m
@@ -11,17 +11,21 @@
 
 int main(int argc, char * argv[]) {
     
-    int var1 = 10;
+    __block int var1 = 10;
     float var2 = 100.0;
     int (^myblock)(void);
     
     myblock = ^int {
+        // not allowed as the variables passed are const by default
+        // var2 = 50.0;
+        var1 = 50;
         return var1 + var2;
     };
     
     int var3 = myblock();
     
     NSLog(@"var3 = %d", var3);
+    NSLog(@"var1 = %d", var1);
     
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
