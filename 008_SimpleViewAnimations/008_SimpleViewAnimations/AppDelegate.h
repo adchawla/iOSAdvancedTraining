58fcef69933183392ec68c1f38b579b98d98119d
Amandeep Chawla <amandeep@adobe.com>
2016-07-26 10:54:58 +0530

    Initial View Controller with Auto Layouting removed

---
+++ 008_SimpleViewAnimations/008_SimpleViewAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  008_SimpleViewAnimations
+//
+//  Created by Amandeep Chawla on 26/07/16.
+//  Copyright © 2016 Amandeep Chawla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 008_SimpleViewAnimations/008_SimpleViewAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  008_SimpleViewAnimations
+//
+//  Created by Amandeep Chawla on 26/07/16.
+//  Copyright © 2016 Amandeep Chawla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
